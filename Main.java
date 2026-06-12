d53df93185a91e3f01759854852c3e9a4fcfca8b
vinay <vinaygadge2003@gmail.com>
2026-06-12 16:30:23 +0530

    Static and Non Static My Understanding

---
+++ Main.java
@@ -0,0 +1,43 @@
+//public class Main {
+//    public static void main(String[] args) {
+//        vinay o=new vinay();
+//        System.out.println(vinay.wow);
+//        System.out.println(o.wow);
+//        System.out.println(o.x);
+//        System.out.println(vinay.x);
+//        Node newnodew=new Node();
+//
+//    }
+//    public void s(){
+//        vinay o=new vinay();
+//        System.out.println(vinay.x);
+//        System.out.println(vinay.wow);
+//        System.out.println(o.x);
+//        System.out.println(o.wow);
+//
+//
+//    }
+//    static class vinay{
+//        int wow=44;
+//        static int x=99;
+//        static int prasanna=10;
+//
+//    }
+//     static class Node{
+//
+//    }
+//
+//}
+
+// static and non static my understandings:
+// there will be only 1 public class
+// there must a one main method for each class
+// if we want nested class that class properties we want to access say in another method which is static
+// then two options all properties in that nested wala class must be static
+// if not we have to create object
+// but if we are creating for any class which is non static in static method it will give error
+// so either make the class static if not all properties static if not
+// do not create it s object inside static method if still not
+// create it as separate class and not as a nested one
+
+
